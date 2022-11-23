72ca3e898136490044b3bb89100bc2c3e6421241
locque <29549891+locque@users.noreply.github.com>
2022-11-23 20:44:38 +0100

    Delete update.bat

--- update.bat
+++
@@ -1,3 +0,0 @@
-powershell -command "Remove-Item update.zip"
-powershell -command "Invoke-WebRequest https://github.com/locque/battlefield-2-mods/releases/download/default/update.zip -OutFile update.zip"
-powershell -command "Expand-Archive -Force '%~dp0update.zip' '%~dp0'"
